84eaaf4fee22c1ffa0bda92346d46d6b0010e4be
Hossein <amk1011689@student.hamk.fi>
2025-10-29 11:53:40 +0200

    Kysyn käyttäjältä mainissä kohde ja lämpötila. Sanotaan loppussa onko kohteella kuumetta vai ei tulostaKuumerajat-metodissa!

--- src/App.java
+++ src/App.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Valitse kohde. Kirjoita ihminen, koira tai hevonen.");
+        String kohde = scan.nextLine();
+        System.out.println("Anna lampotila.");
+        int lampo = Integer.parseInt(scan.nextLine());
+
         tulostaKuumerajat();
+        onkoKuumetta(kohde, lampo);
+
     }
 
     public static void tulostaKuumerajat() {
@@ -11,11 +21,29 @@ public class App {
 
     public static boolean onkoKuumetta(String kohde, int lampo) {
         switch (kohde.toLowerCase()) {
+
             case "ihminen":
+                if (lampo >= 38) {
+                    System.out.println("Kohteella on kuumetta.");
+                } else {
+                    System.out.println("Kohteella ei ole kuumetta.");
+                }
                 return lampo >= 38;
+
             case "koira":
+                if (lampo >= 39) {
+                    System.out.println("Kohteella on kuumetta.");
+                } else {
+                    System.out.println("Kohteella ei ole kuumetta.");
+                }
                 return lampo >= 39;
+
             case "hevonen":
+                if (lampo >= 38) {
+                    System.out.println("Kohteella on kuumetta.");
+                } else {
+                    System.out.println("Kohteella ei ole kuumetta.");
+                }
                 return lampo >= 38;
 
             default:
