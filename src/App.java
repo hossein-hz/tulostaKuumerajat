62eef29264f65d9d99f18d975b9103b9fd189218
Hossein <amk1011689@student.hamk.fi>
2025-10-29 11:00:10 +0200

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        tulostaKuumerajat();
+    }
+
+    public static void tulostaKuumerajat() {
+        System.out.println("Ihminen 37");
+        System.out.println("Koira 39");
+        System.out.println("Hevonen 38");
+    }
+}
